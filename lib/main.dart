2d3f827862ea92fae91f4d4a1542b0de6745779d
williamtessa27 <williamtessa270794@gmail.com>
2023-01-03 16:26:46 +0000

    Market V1

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:maclay_multi_store/views/buyers/main_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Brand-Bold'),
+      home: MainScreen(),
+    );
+  }
+}
---
+++ lib/views/buyers/main_screen.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:maclay_multi_store/views/buyers/nav_screens/account_screen.dart';
+import 'package:maclay_multi_store/views/buyers/nav_screens/cart_screen.dart';
+import 'package:maclay_multi_store/views/buyers/nav_screens/category_screen.dart';
+import 'package:maclay_multi_store/views/buyers/nav_screens/home_screen.dart';
+import 'package:maclay_multi_store/views/buyers/nav_screens/search_screen.dart';
+import 'package:maclay_multi_store/views/buyers/nav_screens/store_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _PageIndex = 0;
+
+  List<Widget> _Pages = [
+    HomeScreen(),
+    CategoryScreen(),
+    StoreScreen(),
+    CartScreen(),
+    SearchScreen(),
+    AccountScreen()
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          currentIndex: _PageIndex,
+          onTap: (value) {
+            setState(() {
+              _PageIndex = value;
+            });
+          },
+          unselectedItemColor: Colors.black,
+          selectedItemColor: Colors.yellow.shade900,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: 'HOME',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/explore.svg',
+                width: 20,
+              ),
+              label: 'CATEGORIES',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/shop.svg',
+                width: 20,
+              ),
+              label: 'STORE',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/cart.svg',
+                width: 20,
+              ),
+              label: 'CART',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/search.svg',
+                width: 20,
+              ),
+              label: 'SEARCH',
+            ),
+            BottomNavigationBarItem(
+              icon: SvgPicture.asset(
+                'assets/icons/account.svg',
+                width: 20,
+              ),
+              label: 'ACCOUNT',
+            ),
+          ]),
+      body: _Pages[_PageIndex],
+    );
+  }
+}
